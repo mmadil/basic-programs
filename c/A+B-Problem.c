3b5a817c313934145d3d25b8dbeca15d3d1415c5
Mohammad Adil <mmadil_14@yahoo.com>
2013-09-30 13:47:34 +0530

    added few more c programs

---
+++ c/A+B-Problem.c
@@ -0,0 +1,13 @@
+/*
+ * Problem 1000 from pog.org
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  printf("%d\n",a+b);
+}
---
+++ shell/wgetsite.sh
@@ -0,0 +1,6 @@
+echo "Enter site URL : "
+read URL
+
+echo "Downloading ..."
+
+# wget -w 2 -r -np -k -p $URL
